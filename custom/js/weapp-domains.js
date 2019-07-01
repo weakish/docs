fe5e0ba5b8e9123d3d96f2f1cbbefeff2b614f7d
leeyeh <lee55962698@gmail.com>
2019-07-01 16:36:38 +0800

    fix: use ES5 syntax

--- custom/js/weapp-domains.js
+++ custom/js/weapp-domains.js
@@ -1,83 +1,82 @@
 angular.module('app').controller('WeappDomainsCtrl', [
-    '$http',
-    '$scope',
-    '$rootScope',
-    '$timeout',
-    '$compile',
-    function ($http, $scope, $rootScope, $timeout, $compile) {
-        $scope.domains = {
-          request: ['正在获取配置'],
-          upload: '正在获取配置',
-          download: '正在获取配置'
-        };
+  '$http',
+  '$scope',
+  '$rootScope',
+  '$timeout',
+  '$compile',
+  function ($http, $scope, $rootScope, $timeout, $compile) {
+      $scope.domains = {
+        request: ['正在获取配置'],
+        upload: '正在获取配置',
+        download: '正在获取配置'
+      };
 
-        const APP_ROUTER_DOMAIN = 'app-router.leancloud.cn';
+      const APP_ROUTER_DOMAIN = 'app-router.leancloud.cn';
 
-        $scope.$watch('pageState.currentApp',function(){
-          var currentApp = $rootScope.pageState.currentApp;
-          console.log($rootScope.pageState.currentApp);
-          $scope.domains.request = [];
-          if (currentApp) {
-            $http.get(`/1.1/clients/self/apps/${currentApp.app_id}/platformCustomDomains`).then(data => {
-              if (data.data && data.data.length) {
-                $scope.domains.request.push(...data.data.map(domainInfo => domainInfo.domain));
-              } else {
-                return $http.get('https://app-router.leancloud.cn/2/route?appId=' + currentApp.app_id).then(data => {
-                  const {
-                    api_server,
-                    engine_server,
-                    rtm_router_server
-                  } = data.data;
-                  $scope.domains.request.push(...[APP_ROUTER_DOMAIN, api_server, engine_server, rtm_router_server]);
-                })
-              }
-            }).catch(e => {
-              $scope.domains.request.push('获取 request 域名异常')
-            })
-
-            $scope.domains.request = $scope.domains.request;
-
-            AV.applicationId = undefined;
-            AV.applicationKey = undefined;
-            AV.init({
-              appId: currentApp.app_id,
-              appKey: currentApp.app_key,
-              masterKey: currentApp.master_key,
-            });
-            var file = new AV.File('weapp-domains-generator-test-file.txt', {
-              base64: 'ZmVlbCBmcmVlIHRvIGRlbGV0ZSB0aGlzIGZpbGUu',
-            });
-            file._fileToken(undefined, {
-              useMasterKey: true
-            }).then(function(uploadInfo) {
-              console.log(uploadInfo);
-              var downloadDomain;
-              var result = uploadInfo.url.match(/\:\/\/([^\/]*)/);
-              if (result) {
-                downloadDomain = result[1].toLowerCase();
-              } else {
-                downloadDomain = '获取文件域名异常';
-              }
-              $scope.domains.download = downloadDomain;
-              var uploadDomain;
-              var result = uploadInfo.upload_url.match(/\:\/\/([^\/]*)/);
-              if (result) {
-                uploadDomain = result[1].toLowerCase();
-              } else {
-                uploadDomain = '获取文件域名异常';
-              }
-              $scope.domains.upload = uploadDomain;
-              $scope.$digest();
-              file.id = uploadInfo.objectId;
-              return file.destroy({
-                useMasterKey: true
+      $scope.$watch('pageState.currentApp',function(){
+        var currentApp = $rootScope.pageState.currentApp;
+        console.log($rootScope.pageState.currentApp);
+        $scope.domains.request = [];
+        if (currentApp) {
+          $http.get('/1.1/clients/self/apps/' + currentApp.app_id + '/platformCustomDomains').then(function (data) {
+            if (data.data && data.data.length) {
+              data.data.map(function(domainInfo) {
+                $scope.domains.request.push(domainInfo.domain);
               });
-            }).catch(function(error) {
-              console.error(error);
-              $scope.error = error;
-            });
-          }
-        });
+            } else {
+              return $http.get('https://app-router.leancloud.cn/2/route?appId=' + currentApp.app_id).then(function (data) {
+                $scope.domains.request = [APP_ROUTER_DOMAIN, data.data.api_server, data.data.engine_server, data.data.rtm_router_server];
+              })
+            }
+          }).catch(function() {
+            console.error(error);
+            $scope.error = error;
+            $scope.domains.request.push('获取 request 域名异常')
+          })
 
-    }
+          $scope.domains.request = $scope.domains.request;
+
+          AV.applicationId = undefined;
+          AV.applicationKey = undefined;
+          AV.init({
+            appId: currentApp.app_id,
+            appKey: currentApp.app_key,
+            masterKey: currentApp.master_key,
+          });
+          var file = new AV.File('weapp-domains-generator-test-file.txt', {
+            base64: 'ZmVlbCBmcmVlIHRvIGRlbGV0ZSB0aGlzIGZpbGUu',
+          });
+          file._fileToken(undefined, {
+            useMasterKey: true
+          }).then(function(uploadInfo) {
+            console.log(uploadInfo);
+            var downloadDomain;
+            var result = uploadInfo.url.match(/\:\/\/([^\/]*)/);
+            if (result) {
+              downloadDomain = result[1].toLowerCase();
+            } else {
+              downloadDomain = '获取文件域名异常';
+            }
+            $scope.domains.download = downloadDomain;
+            var uploadDomain;
+            var result = uploadInfo.upload_url.match(/\:\/\/([^\/]*)/);
+            if (result) {
+              uploadDomain = result[1].toLowerCase();
+            } else {
+              uploadDomain = '获取文件域名异常';
+            }
+            $scope.domains.upload = uploadDomain;
+            $scope.$digest();
+            file.id = uploadInfo.objectId;
+            return file.destroy({
+              useMasterKey: true
+            });
+          }).catch(function(error) {
+            console.error(error);
+            $scope.error = error;
+          });
+        }
+      });
+
+  }
 ]);
